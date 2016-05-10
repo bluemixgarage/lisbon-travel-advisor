02dfd123aff79e4b89aa1cfab75982ed8613aa22
Nicolas Comète <garage@fr.ibm.com>
2016-05-10 18:54:12 +0200

    Revert "First simple custo"
    This reverts commit e1035f1201fd137c3a488e6d3288a3217f16b2c3.

--- app.js
+++ app.js
@@ -16,69 +16,66 @@
 
 'use strict';
 
-var express = require('express')
-    , app = express()
-    , request = require('request')
-    , bluemix = require('./config/bluemix')
-    , extend = require('util')._extend;
+var express  = require('express'),
+  app        = express(),
+  request    = require('request'),
+  bluemix    = require('./config/bluemix'),
+  extend     = require('util')._extend;
 
 // Bootstrap application settings
 require('./config/express')(app);
 
 // if bluemix credentials exists, then override local
-var credentials = extend({
-    url: 'https://gateway.watsonplatform.net/dialog/api'
-    , username: 'ea18801b-9656-4f84-865a-17c2cac0e7d6'
-    , password: 'n6GdxZyXEkTF'
+var credentials =  extend({
+  url: 'https://gateway.watsonplatform.net/dialog/api',
+  username: '<username>',
+  password: '<password>'
 }, bluemix.getServiceCreds('dialog')); // VCAP_SERVICES
 
 if (credentials.url.indexOf('/api') > 0)
-    credentials.url = credentials.url.substring(0, credentials.url.indexOf('/api'));
+  credentials.url = credentials.url.substring(0, credentials.url.indexOf('/api'));
 
 // HTTP proxy to the API
-app.use('/proxy', function (req, res) {
-    var newUrl = credentials.url + req.url;
-    req.pipe(request({
-        url: newUrl
-        , auth: {
-            user: credentials.username
-            , pass: credentials.password
-            , sendImmediately: true
-        }
-    }, function (error) {
-        if (error)
-            res.status(500).json({
-                code: 500
-                , error: errorMessage
-            });
-    })).pipe(res);
+app.use('/proxy', function(req, res) {
+  var newUrl = credentials.url + req.url;
+  req.pipe(request({
+    url: newUrl,
+    auth: {
+      user: credentials.username,
+      pass: credentials.password,
+      sendImmediately: true
+    }
+  }, function(error){
+    if (error)
+      res.status(500).json({code: 500, error: errorMessage});
+  })).pipe(res);
 });
 
 // render index page
-app.get('/', function (req, res) {
-    res.render('index');
+app.get('/', function(req, res) {
+  res.render('index');
 });
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.code = 404;
-    err.message = 'Not Found';
-    next(err);
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.code = 404;
+  err.message = 'Not Found';
+  next(err);
 });
 
 // 500 error message
 var errorMessage = 'There was a problem with the request, please try again';
 
 // non 404 error handler
-app.use(function (err, req, res, next) {
-    var error = {
-        code: err.code || 500
-        , error: err.message || err.error || errorMessage
-    };
+app.use(function(err, req, res, next) {
+  var error = {
+    code: err.code || 500,
+    error: err.message || err.error || errorMessage
+  };
 
-    console.log('error:', error);
-    res.status(error.code).json(error);
+  console.log('error:', error);
+  res.status(error.code).json(error);
 });
 
 var port = process.env.VCAP_APP_PORT || 3000;
